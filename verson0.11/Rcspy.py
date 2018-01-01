e3bc550be8f48ea88b70b1244c77e6ac8a4ce767
Veasonwang <1517138852@qq.com>
2018-01-01 22:21:02 +0800

    更改了交互界面的分辨率

--- verson0.11/Rcspy.py
+++ verson0.11/Rcspy.py
@@ -12,7 +12,7 @@ from matplotlib.figure import Figure
 import sys
 import random
 class MplCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=10, height=4, dpi=100):
+    def __init__(self, parent=None, width=18, height=10, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
 
