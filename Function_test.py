c7ad2ae2df1b7dd0af08836e9e67aa9f2110a5a7
Veasonwang <1517138852@qq.com>
2018-01-30 17:44:24 +0800

    fix bug

--- Function_test.py
+++ Function_test.py
@@ -1,24 +1,12 @@
 #coding=utf-8
-from multiprocessing import Pool
-from threading import Thread
+from obspy import *
+from obspy.core import Stream
+a=read("/home/veason/Desktop/data_for_debug/YN.201506152046.0002.seed")
+n=a[0].copy()
+inv=read_inventory("/home/veason/Desktop/data_for_debug/YN.201506152046.0002.seed")
+inv.plot(projection="ortho", label=False,
+         color_per_network=True)
+#.remove_response(inventory=inv)
+n.plot()
 
-from multiprocessing import Process
-def loop(num):
-    while True:
-        num=num+1
-        print num
-        pass
-class a:
-    def __init__(self):
-        num=0
-        for i in range(3):
-            t = Process(target=loop(num))
-            t.start()
-
-        while True:
-            pass
-
-if __name__ == '__main__':
-    a=['0','1','2']
-    print a[0:1]
 
